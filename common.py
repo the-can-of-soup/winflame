53576258ca1f319b92cfdf132a8210b180c137bd
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-07-23 00:32:14 -0400

    Add `common.py`

---
+++ common.py
@@ -0,0 +1,6 @@
+# CONSTANTS
+
+# Some of these constants have equivalents in "pyproject.toml" that should stay up-to-date with these.
+PROGRAM_NAME: str = 'winflame'
+VERSION: str = '1.0.0'
+PROGRAM_DESCRIPTION: str = 'Creates flame graphs to visualize file storage space.'
