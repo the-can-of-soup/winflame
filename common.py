11a09f3008b2466dbfef51cbb0e27bf2e41a02dc
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-07-23 02:46:45 -0400

    Change "Creates" to "Create" in description

--- common.py
+++ common.py
@@ -3,4 +3,4 @@
 # Some of these constants are mirrored in "pyproject.toml"
 PROGRAM_NAME: str = 'winflame'
 VERSION: str = '1.0.0'
-PROGRAM_DESCRIPTION: str = 'Creates flame graphs to visualize file storage space.'
+PROGRAM_DESCRIPTION: str = 'Create flame graphs to visualize file storage space.'
