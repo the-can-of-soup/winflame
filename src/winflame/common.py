2aa57c06b7df59421d58700475850ed22dd61800
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-07-28 00:07:55 -0400

    Bump version to 1.1.2

--- src/winflame/common.py
+++ src/winflame/common.py
@@ -7,7 +7,7 @@ Common globals for WinFlame.
 # CONSTANTS
 
 PROGRAM_NAME: str = 'winflame'
-PROGRAM_VERSION: str = '1.1.1'
+PROGRAM_VERSION: str = '1.1.2'
 PROGRAM_DESCRIPTION: str = 'Create flame graphs to visualize file storage space.'
 
 
